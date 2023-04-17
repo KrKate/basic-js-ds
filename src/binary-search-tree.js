4c562f567be3d44c51206ac9f94f8521095c9c3c
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-04-18 02:14:37 +0500

    fix:fix

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -19,6 +19,8 @@ class BinarySearchTree {
 constructor () {
   this.rootNode = null;
 }
+
+
   root() {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
@@ -73,34 +75,11 @@ constructor () {
   find(data) {
     //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    let current = this.rootNode;
-
-    while (current) {
-      if (data < current.data) {
-        current = current.left;
-      } else if (data > current.data) {
-        current = current.right;
-      } else {
-        return current;
-      }
-    }
   }
 
   remove(data) {
     //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    let current = this.rootNode;
-    let parent = null;
-
-    while (current) {
-      if (data < current.data) {
-        current = current.left;
-        parent = current;
-      } else if (data> current.data) {
-        current = current.right;
-        parent = current
-      }
-    }
   }
 
   min() {
