7a15d7edbc1c0c6197a3ac6a5c23d0988091c4c8
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-04-17 17:19:14 +0500

    feat: solve stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.stack = []
+  }
+  push(element) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.stack.length > 0) {
+      return this.stack.pop()
+    }
   }
-
+// peek возвращает последний добавленный элемент
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.stack.length > 0)
+    return this.stack.slice(-1)[0]
   }
 }
 
